8aed3466eb30bcf9e452243a4f15b0d3931028a6
Michael <michael.vicente1705@gmail.com>
2024-03-04 21:40:22 -0300

    Enviando mais uma vez e de novo

---
+++ ArrayProduto/Produto.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArrayProduto
+{
+    public class Produto
+    {
+        public int codigo;
+        public string? nome;
+        public double preco;
+
+        //declaração dos métodos
+        public void MostrarAtributos()
+        {
+            Console.WriteLine($"Código: {codigo} \tNome: {nome} \tPreço: {preco:c}");
+        }
+        public void CalcularAumento(double porcentagem)
+        {
+            preco = preco + (preco * porcentagem/100);
+            //preco += preco * porcentagem/100;
+        }
+    }
+}
---
+++ ArrayProduto/Program.cs
@@ -0,0 +1,27 @@
+﻿using ArrayProduto;
+
+Produto[] vetP = new Produto[3];
+for(int i = 0; i < vetP.Length; i++)
+{
+    //instanciar cada possição do vetor
+    vetP[i] = new Produto();
+    Console.Write("Digite o código: ");
+    vetP[i].codigo= Convert.ToInt32(Console.ReadLine());
+    Console.Write("Digite o nome: ");
+    vetP[i].nome= Console.ReadLine();
+    Console.Write("Digite o preço: ");
+    vetP[i].preco= Convert.ToDouble(Console.ReadLine());
+}
+for(int i = 0; i < vetP.Length; i++)
+    vetP[i].MostrarAtributos();
+
+foreach (Produto p in vetP)
+    p.MostrarAtributos();
+
+Console.WriteLine("Informe a porcentagem de aumento: ");
+double percentual = Convert.ToDouble(Console.ReadLine());
+for(int i = 0; i < vetP.Length; i++)
+{
+    vetP[i].CalcularAumento(percentual);   
+    vetP[i].MostrarAtributos();
+}
