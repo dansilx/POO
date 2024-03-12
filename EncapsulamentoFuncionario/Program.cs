111af22b68e66776f6dc98cca74970c923b4b4be
Michael <michael.vicente1705@gmail.com>
2024-03-11 22:10:18 -0300

    salvando]

---
+++ EncapsulamentoFuncionario/Program.cs
@@ -0,0 +1,11 @@
+﻿using EncapsulamentoFuncionario;
+
+Funcionario f1 = new Funcionario();
+
+f1.Codigo = 1;
+f1.Nome = "Danilo";
+f1.Salario = 1500.00;
+
+Console.WriteLine($"Código: {f1.Codigo}");
+Console.WriteLine($"Nome: {f1.Nome}");
+Console.WriteLine($"Salario: {f1.Salario}");
