cb53f709c7d87e6566cace71001b213d453a621f
Michael <michael.vicente1705@gmail.com>
2024-03-18 21:45:42 -0300

    salvo

--- ConstrutorProduto/Produto.cs
+++ ConstrutorProduto/Produto.cs
@@ -7,6 +7,28 @@ namespace ConstrutorProduto
 {
     public class Produto
     {
-        
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public static int Quantidade { get; set; }
+
+        public Produto(int codigo)
+        {
+            Codigo = codigo;
+            Quantidade ++;
+        }
+
+        public Produto(string nome)
+        {
+            Nome = nome;
+            Quantidade ++;
+        }
+
+        public Produto(double preco)
+        {
+            Preco = preco;
+            Quantidade ++;
+        }
     }
 }
--- ConstrutorProduto/Program.cs
+++ ConstrutorProduto/Program.cs
@@ -1,2 +1,9 @@
-﻿using ConstrutorProduto();
+﻿using ConstrutorProduto;
 
+Produto p1 = new Produto(2);
+
+Produto p2 = new Produto("Ana");
+
+Produto p3 = new Produto(5.5);
+
+Console.WriteLine("Quantidade de instâncias " + Produto.Quantidade);
