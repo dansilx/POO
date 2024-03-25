7358546c7ee5207dfba36c74b61c9a83ff656fbb
dansilx <danilosilveira@msn.com>
2024-03-25 20:38:17 -0300

    salvar

---
+++ AgregacaoConta/Program.cs
@@ -0,0 +1,15 @@
+﻿//Função Main
+
+using AgragacaoConta;
+
+Endereco end = new Endereco("Rua Fatec", 75, "PP");
+
+Cliente cli = new Cliente("Ana", "1111", "1111-11");
+cli.Ender = end; //agregando o endereço no cliente 
+
+Conta c1 = new Conta();
+c1.Numero = 1;
+c1.Titular = cli;
+c1.Saldo = 100;
+
+Console.WriteLine(c1.Titular.Logradouro);
