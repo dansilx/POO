c0f790da3bb3e5054ec9def841491cd8f6986904
dansilx <danilosilveira@msn.com>
2024-04-29 19:41:03 -0300

    salvar

---
+++ HerancaProduto/Perecivel.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HerancaProduto
+{
+    public class Perecivel : Produto
+    {   
+        private string dataFabricacao;
+        private string dataValidade;
+        private int lote;
+
+        public string dataFabricacao {
+            get{return dataFabricacao;}
+            set{dataFabricacao = value;}
+        } 
+
+        public string DataValidade {
+            get{return dataValidade;}
+            set{dataValidade = value;}
+        }
+
+        public int Lote {
+            get{return lote;}
+            set{lote = value;}
+        }
+    }
+}
