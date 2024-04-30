f1d2be51d384054fe76370a710d7fcfb18bbead3
dansilx <danilosilveira@msn.com>
2024-04-29 21:26:50 -0300

    salvar

---
+++ PolimorfismoFuncionario/Diretor.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PolimorfismoFuncionario
+{
+    public class Diretor
+    {
+         public Diretor (int codigo, string nome, double salario) : base (codigo, nome, salario) {
+        
+        }
+    }
+}
---
+++ PolimorfismoFuncionario/Gerente.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PolimorfismoFuncionario
+{
+    public class Gerente
+    {
+        public Gerente (int codigo, string nome, double salario) : base (codigo, nome, salario) {
+        
+        }
+    }
+}
