a8a1d707ab295aa77932043be8084425fbf40bb5
dansilx <danilosilveira@msn.com>
2024-05-13 20:12:05 -0300

    enviando a aula

--- AbstratoCliente/Program.cs
+++ AbstratoCliente/Program.cs
@@ -5,5 +5,8 @@ using AbstratoCliente;
 //Cliente c = new Cliente(); //não instanciará
 
 ClienteFisico cf = new ClienteFisico(1, "Ana", "Rua Feliz", "12345678910");
-cf.Mostrar();
 
+ClienteJuridico cj = new ClienteJuridico(100, "Empresa", "Rua Amarela", "987654321000112");
+
+cf.Mostrar();
+cj.Mostrar();
