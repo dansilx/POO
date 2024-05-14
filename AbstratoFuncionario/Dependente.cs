eecdfc9de95ee4374def87ab68ef99d714477349
dansilx <danilosilveira@msn.com>
2024-05-13 22:19:04 -0300

    salvar

---
+++ AbstratoFuncionario/Dependente.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbstratoFuncionario
+{
+    public class Dependente
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
