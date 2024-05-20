62e3dd578ea8837520ca321adfcffaf783d61d9c
dansilx <danilosilveira@msn.com>
2024-05-20 20:33:32 -0300

    salvar

--- HerancaFuncionario/obj/Debug/net6.0/HerancaFuncionario.AssemblyInfo.cs
+++ HerancaFuncionario/obj/Debug/net6.0/HerancaFuncionario.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HerancaFuncionario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f68418c0d21ec27866f4aa19e38edb9c6b4d50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df0c8b7de078293221fce9c871f92f2216b04e05")]
 [assembly: System.Reflection.AssemblyProductAttribute("HerancaFuncionario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HerancaFuncionario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
