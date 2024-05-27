fdeceb6c13c24b4c42fbae52a257b7d6d06f1179
dansilx <danilosilveira@msn.com>
2024-05-26 22:08:51 -0300

    salvar

---
+++ InterfaceTributo/ContaCorrente.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterfaceTributo
+{
+    public class ContaCorrente : ITributavel
+    {
+        public int Saldo { get; set; }
+
+        public double calculaTributos() {
+            return this.Saldo * 5/100;
+        }
+    }
+}
